9bf1bc34a100d55256c43282346b52337fb0f4e3
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-30 07:34:30 +0000

    Add checkAuthentication middleware to auth route
    This middleware checks if the user is authenticated before allowing
    access to certain routes. If the user is already authenticated, they
    will be redirected to the home page. This helps to prevent
    authenticated users from accessing the login page.

--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,23 @@ const configurePassport = require("../configurePassport");
 const router = express.Router();
 configurePassport(passport);
 
+// Check if the user is authenticated
+function checkAuthentication(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+// Signup routes
 router.get("/sign-up", authController.controlSignUpGet);
 router.post("/sign-up", authController.controlSignUpPost);
 
-router.get("/log-in", authController.controlLogInGet);
+// Login routes
+router.get("/log-in", checkAuthentication, authController.controlLogInGet);
 router.post(
   "/log-in",
+  checkAuthentication,
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/log-in",
@@ -20,6 +31,7 @@ router.post(
   })
 );
 
+// Logout route
 router.get("/log-out", authController.controlLogOutGet);
 
 module.exports = router;
